d5a8ebe1e347278e156272b98c9abd8512f890ef
xDevansh <devjollani@Gmail.com>
2026-03-21 20:13:15 +0530

    -tested multiplytwo template

--- templatesm1p2.cpp
+++ templatesm1p2.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 
 template <typename T>
-T getMax(T a,T b)
+auto multiplyTwo(T a,T b)
 {
-    return (a > b) ? a : b;
+    return a*b;
 }
 
 int main()
 {
-    std::cout<<"Max of '2' and '5' is: "<<getMax<int>(2,5)<<'\n';
-    std::cout<<"Max of '2.5' and '5.5' is: "<<getMax<float>(2.5,5.5)<<'\n';
-    std::cout<<"Max of 'a' and 'a' is: "<<getMax('a','z')<<'\n';
+    std::cout<<"Multiplication of '2' and '5' is: "<<multiplyTwo<int>(2,5)<<'\n';
+    std::cout<<"Multiplication of '2.5' and '5.5' is: "<<multiplyTwo(2.5,5.5)<<'\n';
+    std::cout<<"Multiplication of 'a' and 'z' is: "<<multiplyTwo('a','z')<<'\n';
 
     return 0;
     
