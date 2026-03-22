a403b84f55d70b9588f657bc6e1f6a10adf5a916
xDevansh <devjollani@Gmail.com>
2026-03-22 22:16:14 +0530

    -used make pair to make a pair in ssingle line

--- pairm3p1.cpp
+++ pairm3p1.cpp
@@ -4,10 +4,7 @@
 
 int main()
 {
-    std::pair<int,std::string> myPair;
-
-    myPair.first=100;
-    myPair.second="Heba";
+    auto myPair = std::make_pair(100,"Heba");
 
     std::cout<<"first element of pair: "<<myPair.first<<'\n'<<"second element of pair :"<<myPair.second<<std::endl;
 
