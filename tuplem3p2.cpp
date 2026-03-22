50f7776244c5cf6c39ac57a189c40e6d63d52730
xDevansh <devjollani@Gmail.com>
2026-03-22 23:23:12 +0530

    -used structured binding to write a more cleaner way to assign or access tuples or pairs

--- tuplem3p2.cpp
+++ tuplem3p2.cpp
@@ -12,11 +12,12 @@ std::ostream& operator<<(std::ostream& os,const std::pair<int,std::string>& pair
 int main()
 {
     std::tuple myTuple{2,234,"hello"};
-
+/* 
     int elem0 = std::get<0>(myTuple);
     int elem1 = std::get<1>(myTuple);
     std::string elem2 = std::get<2>(myTuple);
-
+ */
+    auto [elem0,elem1,elem2] = myTuple;
     std::cout << "elem0: " << elem0 << ", elem1: " << elem1 << ", elem2: "<<elem2<<"\n";
 
 
