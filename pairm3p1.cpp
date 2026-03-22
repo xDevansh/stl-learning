0b8ff0904345e73eccc54d67a1be42cc958e5be9
xDevansh <devjollani@Gmail.com>
2026-03-22 22:38:06 +0530

    -comapred two pairs

--- pairm3p1.cpp
+++ pairm3p1.cpp
@@ -2,11 +2,21 @@
 #include<utility>
 #include<string>
 
+std::pair<int,std::string> comparePairs(const std::pair<int,std::string>& pair1, const std::pair<int,std::string>& pair2)
+{
+    return (pair1>pair2) ? pair1:pair2;
+}
+
+
 int main()
 {
     auto myPair = std::make_pair(100,"Heba");
 
-    std::cout<<"first element of pair: "<<myPair.first<<'\n'<<"second element of pair :"<<myPair.second<<std::endl;
+    std::pair myPair2{200,"Veba"};
+    std::cout<<"first element of pair: "<<myPair.first<<'\n'<<"second element of pair :"<<myPair.second<<'\n';
+
+    auto winner = comparePairs(myPair,myPair2);
+    std::cout<<"The pair: ("<<winner.first<<","<<winner.second<< ") wins"<<std::endl;
 
     return 0;
 }
