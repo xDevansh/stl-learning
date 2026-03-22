8f5d7c4647d6cb3f8c94192e28afa3871f121727
xDevansh <devjollani@Gmail.com>
2026-03-22 23:19:06 +0530

    -tuple examples

---
+++ tuplem3p2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<tuple>
+#include<string>
+
+/* 
+std::ostream& operator<<(std::ostream& os,const std::pair<int,std::string>& pair)
+{
+    return os<<pair.first<<","<<pair.second;
+}
+ */
+
+int main()
+{
+    std::tuple myTuple{2,234,"hello"};
+
+    int elem0 = std::get<0>(myTuple);
+    int elem1 = std::get<1>(myTuple);
+    std::string elem2 = std::get<2>(myTuple);
+
+    std::cout << "elem0: " << elem0 << ", elem1: " << elem1 << ", elem2: "<<elem2<<"\n";
+
+
+}
