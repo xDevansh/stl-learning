bc86afacd6d1a30745c63e95eb22ff7868636984
xDevansh <devjollani@Gmail.com>
2026-03-23 18:23:48 +0530

    -explored way to initialize a vector

---
+++ vectorm4p1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+int main()
+{
+    std::vector<int> v1;
+
+    std::vector<int> v2(5);
+
+    std::vector<int> v3(5,10);
+
+    std::vector<int> v4{1,2,3,4};
+    return 0;
+}
