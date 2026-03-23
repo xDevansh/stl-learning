2832fae8252c8283260e9335ac4bc7b1a063c3a4
xDevansh <devjollani@Gmail.com>
2026-03-23 19:51:36 +0530

    -tried out ize() and capacity()

--- vectorm4p1.cpp
+++ vectorm4p1.cpp
@@ -1,14 +1,26 @@
 #include<iostream>
 #include<vector>
+#include<ranges>
 
 int main()
 {
     std::vector<int> v1;
 
-    std::vector<int> v2(5);
+    /* std::vector<int> v2(5);
 
     std::vector<int> v3(5,10);
 
     std::vector<int> v4{1,2,3,4};
     return 0;
+
+     */
+
+    std::cout<<"SIZE: "<<v1.size()<<'\n'<<"CAPACITY: "<<'\n';
+
+    for(int i: std::views::iota(1,16))
+    {
+        v1.push_back(i);
+        std::cout<<"itertion :"<<i<<'\n';
+        std::cout<<"SIZE: "<<v1.size()<<'\n'<<"CAPACITY: "<<v1.capacity()<<'\n';
+    }
 }
